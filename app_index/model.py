dd2c295d7cfe95bfadd5479670d588435f5b881c
Christopher Reece <mrkita.reece@gmail.com>
2018-04-03 18:35:22 +0900

    added User and Category Schemas

--- app_index/model.py
+++ app_index/model.py
@@ -6,6 +6,23 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80))
+    email = Column(String(80), nullable=False)
+    picture = Column(String(80))
+    username = Column(String(80))
+    password_hash = Column(String(64))
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+
+
 class Item(Base):
     __tablename__ = 'item'
 
@@ -14,12 +31,12 @@ class Item(Base):
     name = Column(String(80), nullable=False)
     details = Column(String(400))
     picture = Column(String(200))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
 
     # relationships
-    """
-    author = relationship(User)
     category = relationship(Category)
-    """
+    user = relationship(User)
 
 engine = create_engine("sqlite:///catalog.db")
 Base.metadata.create_all(engine)

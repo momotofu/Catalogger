5d965928758319d8e3ef5856fc89350919d61d22
Christopher Reece <mrkita.reece@gmail.com>
2018-04-06 12:31:52 +0900

    renamed app folder and created subcategory tables

--- app/model.py
+++ app/model.py
@@ -17,12 +17,30 @@ class User(Base):
     username = Column(String(80))
     password_hash = Column(String(64))
 
-class Category(Base):
-    __tablename__ = 'category'
+class A0_Categories(Base):
+    __tablename__ = 'a_0_categories'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
-    # TODO: design database for sub-categories
+
+# Intermediary table
+class A0_to_A1_Categories(Base):
+    __tablename__ = 'a0_to_a1_categories'
+
+    id = Column(Integer, primary_key=True)
+    category_id = Column(Integer, ForeignKey('a0_categories.id'))
+    subcategory_id = Column(Integer, ForeignKey('a1_categories.id'))
+
+    # Relationships
+    a0_categories = relationship(A0_Categories)
+    a1_categories = relationship(A1_Categories)
+
+
+class A1_Categories(Base):
+    __tablename__ = 'a_1_categories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
 
 
 class Item(Base):

9fe338d1d0a44014e1ea3226f58b8ebe0684442d
Christopher Reece <mrkita.reece@gmail.com>
2018-05-11 13:54:40 +0900

    wired up isEditing state

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,11 +1,54 @@
-const CategoryList = function(items) {
-    const self = this;
+'use strict';
 
-    self.categories = ko.observableArray(items);
+const ENTER_KEY = 13;
 
-    self.createCategory = function() {
-    };
+// A factory function we can use to create binding handlers for specific
+// keycodes.
+function keyhandlerBindingFactory(keyCode) {
+  return {
+    init: function (element, valueAccessor, allBindingsAccessor, data, bindingContext) {
+      var wrappedHandler, newValueAccessor;
 
-    self.deleteCategory = function() {
+      // wrap the handler with a check for the enter key
+      wrappedHandler = function (data, event) {
+        if (event.keyCode === keyCode) {
+          valueAccessor().call(this, data, event);
+        }
+      };
+
+      // create a valueAccessor with the options that we would want to pass to the event binding
+      newValueAccessor = function () {
+        return {
+          keyup: wrappedHandler
+        };
+      };
+
+      // call the real event binding's init function
+      ko.bindingHandlers.event.init(element, newValueAccessor, allBindingsAccessor, data, bindingContext);
     }
-};
+  };
+}
+
+// a custom binding to handle the enter key
+ko.bindingHandlers.enterKey = keyhandlerBindingFactory(ENTER_KEY);
+
+// ViewModel
+const CategoryList = function(categories) {
+  // map array of passed in categories to an observableArray of category objects
+  this.categories = ko.observableArray(categories)
+
+  this.isEditing = ko.observable(false)
+
+  this.setEditing = function() {
+    this.isEditing(!this.isEditing())
+  }.bind(this)
+
+  this.createCategory = function() {
+  };
+
+  this.deleteCategory = function() {
+  }
+}
+
+
+

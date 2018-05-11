cc231ec4830b86243fcb4e8829949eff47e80afe
Christopher Reece <mrkita.reece@gmail.com>
2018-05-11 18:40:53 +0900

    added active item functionality

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -41,11 +41,14 @@ class Category {
         eval(`this.${prop} = data.${prop}`)
       }
     }
+
   }
 }
 
 // ViewModel
 const CategoryList = function(categories) {
+  const self = this
+
   // map array of passed in categories to an observableArray of category objects
   this.categories = ko.observableArray(categories.map((category) => {
     return new Category(category)
@@ -54,13 +57,22 @@ const CategoryList = function(categories) {
   // state
   this.isEditing = ko.observable(false)
   this.canAdd = ko.observable(false)
+  this.activeCategoryId = ko.observable(-1)
+  this.isActiveClass = function(id) {
+    return this.activeCategoryId() == id ? 'active' : ''
+  }.bind(this)
 
+  // setters
   this.setEditing = function() {
     this.isEditing(!this.isEditing())
   }.bind(this)
 
   this.setCanAdd = function() {
     this.canAdd(!this.canAdd())
+  }.bind(this)
+
+  this.setActiveCategoryId = function(id) {
+    this.activeCategoryId(id)
   }.bind(this)
 
   this.createCategory = function() {

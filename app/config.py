7bbe619d71d52f8dcb437c224b21d4b1e8f02646
Christopher Reece <mrkita.reece@gmail.com>
2018-05-21 11:46:13 +0900

    updated database global variable name

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 class Config(object):
     DEBUG = False
     TESTING = False
-    DATABASE_URI = 'sqlite:///restaurantmenu.db'
+    DATABASE_URI = 'sqlite:///catalog.db'
 
     IMAGE_FOLDER = 'app_index/static/images'
     ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png', 'gif'])

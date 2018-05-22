fe943d11378cd383c670efe6f8c4370cb109f306
Christopher Reece <mrkita.reece@gmail.com>
2018-05-22 14:51:29 +0900

    setup basic create item template

---
+++ app/views/item.py
@@ -0,0 +1,26 @@
+from app.model import Item, Category
+from app.utils.utils import get_session
+from flask import Blueprint, render_template, request
+
+session = get_session('sqlite:///catalog.db')
+category = Blueprint('item',
+                        __name__,
+                        template_folder='templates')
+
+@item.route('/')
+@item.route('/items/<int: category_id>')
+def getItems(category_id):
+    pass
+
+@item.route('/items/<int: category_id>/new', methods=['GET', 'POST'])
+def createItem(category_id):
+    if request.method == 'GET':
+        try:
+            session.query(Category).filter(Category.id == category_id).one()
+            return render_template('item/index.html', category=category)
+
+        except:
+            raise
+
+
+

1d890b8daca657becbcd51f92b336ab5b6776f29
Christopher Reece <mrkita.reece@gmail.com>
2018-05-22 14:51:43 +0900

    update to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env, options) => {
           app: [
             path.resolve(__dirname, 'app/templates/app.js')
           ],
-          vender_css: [
+          venders: [
             path.resolve(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.min.css')
           ],
       },

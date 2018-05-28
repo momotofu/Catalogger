bdddf4609eaac3e35883457b0b23fddb1df9ce87
Christopher Reece <mrkita.reece@gmail.com>
2018-05-28 18:37:55 +0900

    opened up createItem path

--- app/views/item.py
+++ app/views/item.py
@@ -1,6 +1,6 @@
 from app.model import Item, Category
 from app.utils.utils import get_session
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 
 session = get_session('sqlite:///catalog.db')
 item = Blueprint('item',
@@ -8,16 +8,25 @@ item = Blueprint('item',
                         template_folder='templates')
 
 @item.route('/')
-@item.route('/items/<int:category_id>')
+@item.route('/category/<int:category_id>/items')
 def getItems(category_id):
     pass
 
-@item.route('/items/<int:category_id>/new', methods=['GET', 'POST'])
+@item.route('/category/<int:category_id>/items/new', methods=['GET', 'POST'])
 def createItem(category_id):
     if request.method == 'GET':
         try:
             category = session.query(Category).filter(Category.id == category_id).one()
             return render_template('item/index.html', category=category)
+
+        except:
+            raise
+
+    if request.method == 'POST':
+        try:
+            params = request.form
+            print('PARAMS: ', params)
+            return redirect(url_for("category.allCategories"))
 
         except:
             raise

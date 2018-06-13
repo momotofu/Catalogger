4e78db4c1aa162aafa4e9c9d0abbfe5efdb3bed9
Christopher Reece <mrkita.reece@gmail.com>
2018-06-13 12:01:46 +0900

    created signup template and route

--- app/views/login.py
+++ app/views/login.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect
 from flask import url_for, flash
 from flask_login import LoginManager, login_required, logout_user, current_user
+from flask_login import login_user
 from flask_bcrypt import bcrypt
 from .forms import LoginForm
 from app.model import User
@@ -22,7 +23,58 @@ def load_user(user_id):
 
 @login.route('/signup', methods=['POST', 'GET'])
 def user_signup():
-    pass
+    form = LoginForm()
+
+    if request.method == 'GET':
+        return render_template('login/signup.html', form=form)
+
+    elif request.method == 'POST':
+        if form.validate() == False:
+
+            flash('All fields are required')
+
+            return render_template('login/signup.html', form=form)
+
+        else:
+            # generate a password hash from the users' password
+            pw_hash = bcrypt.hashpw(form.password.data.encode(), bcrypt.gensalt())
+            email = form.email.data
+
+            # check to make sure user doesn't already exist
+            try:
+                prev_user = session.query(User).filter(email == email).all()
+
+                if not prev_user:
+                    # create a new user
+                    user = User(
+                            email=email,
+                            password_hash=pw_hash,
+                            authenticated=True)
+
+                    # add user to the database
+                    session.add(user)
+                    session.commit()
+
+                    # store user in session
+                    login_user(user, remember=True)
+
+                    # provide the user feedback
+                    flash('Welcome %s' % user.name)
+
+                    return redirect(url_for('category.allCategories'))
+
+
+                else:
+                    # notify that a user has already been created with that
+                    # email
+                    flash('A user already exists with that email')
+
+                    return redirect('login/signup.html')
+
+            except:
+                session.rollback()
+                raise
+
 
 
 @login.route('/login', methods=['POST', 'GET'])

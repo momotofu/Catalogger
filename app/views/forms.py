a58f35b19d21649749f9695995fb5bcd7f40bf1c
Christopher Reece <mrkita.reece@gmail.com>
2018-06-14 12:19:12 +0900

    fixed flash-message spacing

--- app/views/forms.py
+++ app/views/forms.py
@@ -3,5 +3,8 @@ from wtforms import StringField, PasswordField
 from wtforms import validators
 
 class LoginForm(FlaskForm):
-    email = StringField('email', [validators.required('i.e. john@smith.com')])
+    email = StringField(
+                ('email',
+                [validators.required('i.e. john@smith.com'),
+                validators.Email('Please enter a valid email address')]))
     password = PasswordField('password', [validators.required('password yes')])

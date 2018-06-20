7a48321f02dd39daf3cdc7ef9cff6094d0f2a8d2
Christopher Reece <mrkita.reece@gmail.com>
2018-06-20 13:52:45 +0900

    created getCategories API endpoint

--- app/api/api.py
+++ app/api/api.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, send_from_directory
-from app.utils.utils import get_session
+from flask_login import current_user
 
-session = get_session('sqlite:///catalog.db')
+from app.app import session
+from app.model import Category
+
+import json
+
 api = Blueprint('api',
                         __name__,
                         template_folder='templates')
@@ -12,4 +16,27 @@ api = Blueprint('api',
 @api.route('/images/<filename>')
 def image_file(filename):
     return send_from_directory('static/images', filename)
+
+
+
+@api.route('/categories/JSON')
+def getCategories():
+    if current_user.is_authenticated:
+        # grab user specific categories from the database
+        categories = (
+                session.query(Category)
+                .filter(Category.user_id == current_user.id)
+                .all())
+    else:
+        # grab public categories from the database
+        categories = (
+                session.query(Category)
+                .filter(Category.user_id == None)
+                .all())
+
+    # serialize and prepare category objects for json
+    serialized_categories = [category.serialize for category in categories]
+    serialized_categories.reverse()
+
+    return json.dumps(serialized_categories), 200
 
